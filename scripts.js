33121edc04e9ee88f62cac14d080e9c20f15e200
Nicole Hategekimana <nicoleha127@gmail.com>
2018-12-11 12:17:19 +0200

    calculator made

---
+++ scripts.js
@@ -0,0 +1,24 @@
+ var add = function(number1, number2) {
+      return number1 + number2;
+    };
+   
+
+    var substract = function(number1, number2) {
+        return number1 - number2;
+      };
+      
+
+      var divide = function(number1, number2) {
+        return number1 / number2;
+      };
+      
+
+
+      var multiply = function(number1, number2) {
+        return number1 * number2;
+      };
+     
+      var number1=parseInt(prompt("Enter the first number: "))
+      var number2=parseInt(prompt("Enter the second number: "))
+      var result=multiply(number1,number2)
+      alert(result);
